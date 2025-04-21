d2e47fca5bcf148703a3e2966c896146b3aaa077
javicast55 <javieer55c@gmail.com>
2025-04-21 19:34:06 +0100

    functions completed

--- libft.h
+++ libft.h
@@ -14,6 +14,7 @@
 # define LIBFT_H
 
 #include <stdlib.h>
+#include <unistd.h>
 
 int ft_isalpha(char c);
 int ft_isdigit(char c);
@@ -48,4 +49,10 @@ char	**ft_split(char const *s, char c);
 char    *ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned
 	int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int,
+		char*));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 #endif
